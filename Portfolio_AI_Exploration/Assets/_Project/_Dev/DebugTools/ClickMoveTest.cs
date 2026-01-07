cd75a0678a279017698f5450bfedbd6f69a96e2e
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-01-07 12:39:29 +0900

    feat: add exploration navmesh prototype scene

---
+++ Portfolio_AI_Exploration/Assets/_Project/_Dev/DebugTools/ClickMoveTest.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMoveTest : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
+
