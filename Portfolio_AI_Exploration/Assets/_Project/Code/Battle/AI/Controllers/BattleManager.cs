ac4b9e451cec20f4e2e8a146b47ce73649a934db
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-05-22 18:49:57 +0900

    feat: add BGM playback to battle scene

--- Portfolio_AI_Exploration/Assets/_Project/Code/Battle/AI/Controllers/BattleManager.cs
+++ Portfolio_AI_Exploration/Assets/_Project/Code/Battle/AI/Controllers/BattleManager.cs
@@ -1,4 +1,3 @@
-using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
@@ -13,21 +12,12 @@ public class BattleManager : MonoBehaviour
 
     void Start()
     {
+        AudioManager.Instance.FadeInBGM();
+
+        AudioManager.Instance.PlayBGMIfNotPlaying(BGMName.Battle);
         battleEnded = false;
-
-        Debug.Log(player);
-        Debug.Log(enemy);
-        Debug.Log(hpUI);
-
-        Debug.Log($"[Battle Start] Player HP: {player.Blackboard.CurrentHP}");
-        Debug.Log($"[Battle Start] Enemy  HP: {enemy.Blackboard.CurrentHP}");
-      
-      
         hpUI.Init(player, enemy);
-
-
         BattleResultData.Reset();
-        Debug.Log(BattleResultData.interventionCount.ToString());
         player.SetTarget(enemy);
         enemy.SetTarget(player);
         playerUBGauge.Init(player); 
@@ -41,6 +31,8 @@ public class BattleManager : MonoBehaviour
         {
             CheckBattleEnd();
         }
+
+      
     }
 
     public void StartBattle()
@@ -59,10 +51,13 @@ public class BattleManager : MonoBehaviour
         }
         if (enemy.Blackboard.IsDead)
         {
+   
+
             EndBattle(ResultType.Victory);
         }
         else if (player.Blackboard.IsDead)
         {
+
             EndBattle(ResultType.Defeat);
         }
     }
@@ -70,46 +65,19 @@ public class BattleManager : MonoBehaviour
     void EndBattle(ResultType resultType)
     {
 
+
         if (battleEnded)
         {
             Debug.Log("❌ すでに終了済み");
             return;
         }
 
-
-
-
-        Debug.Log("✅ 初回EndBattle通過");
         battleEnded = true;
 
         BattleResultData.resultType = resultType;
 
-        Debug.Log($"Battle Finished : {resultType}");
-
-
-        Debug.Log("👉 遷移呼び出し前");
-
-        Debug.LogWarning($"Instance: {SceneTransitionManager.Instance}");
-
-        var manager = SceneTransitionManager.Instance;
-        Debug.Log("② Instance取得");
-        Debug.Log($"manager null? {manager == null}");
-
-
-        Debug.LogWarning($"Instance: {SceneTransitionManager.Instance}");
-        Debug.LogWarning($"Scene: {SceneTransitionManager.Instance.gameObject.scene.name}");
-
-
         SceneTransitionManager.Instance.TransitionToNextScene(FadeMode.SimpleColor);
 
-
-
-        Debug.Log("👉 遷移呼び出し後");
     }
-
-
-
-
-
 
 }
