f3f35217eeaaf60261853eaa44fd22c2e367b45c
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-06-19 19:01:40 +0900

    refactor: reorganize project folder structure

--- Portfolio_AI_Exploration/Assets/_Project/Code/UI/Controller/IPlayerInputReceiver.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IPlayerInputReceiver
-{
-    void ReceivePlayerCommand(PlayerCommand command);
-}
--- Portfolio_AI_Exploration/Assets/_Project/_Dev/Notes/RefactorPlan.md
+++
@@ -1,4 +0,0 @@
-■ プロパティ設計統一
-・Max系はget-only
-・ScriptableObjectは[SerializeField] private + public getter
-・Blackboardは外部書き換え不可
